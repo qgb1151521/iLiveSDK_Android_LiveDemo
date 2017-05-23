0d92c419a665239dbeb7bafd3ded4e4c431d9f01
xkazer <xkazer@sina.com>
2017-05-23 19:33:08 +0800

    添加ILiveSDK Demo基础代码

---
+++ app/src/main/java/com/tencent/ilivedemo/DemoApp.java
@@ -0,0 +1,25 @@
+package com.tencent.ilivedemo;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.tencent.ilivesdk.ILiveSDK;
+import com.tencent.livesdk.ILVLiveConfig;
+import com.tencent.livesdk.ILVLiveManager;
+import com.tencent.qalsdk.sdk.MsfSdkUtils;
+
+/**
+ * Created by xkazerzhang on 2017/5/23.
+ */
+public class DemoApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if(MsfSdkUtils.isMainProcess(this)){
+            // 初始化LiveSDK
+            ILiveSDK.getInstance().initSdk(this, 1400028096, 11851);
+            ILVLiveManager.getInstance().init(new ILVLiveConfig());
+        }
+    }
+}
---
+++ app/src/main/java/com/tencent/ilivedemo/ui/MenuActivity.java
@@ -0,0 +1,20 @@
+package com.tencent.ilivedemo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.tencent.ilivedemo.R;
+
+/**
+ * 示例菜单
+ */
+public class MenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_mainmenu);
+    }
+
+}
