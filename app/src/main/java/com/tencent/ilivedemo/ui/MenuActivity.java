cf2ec4ba7cda8d35ffc5958888d9186ee70b2266
xkazer <xkazer@sina.com>
2017-05-24 20:19:37 +0800

    添加开播demo

--- app/src/main/java/com/tencent/ilivedemo/ui/MenuActivity.java
+++ app/src/main/java/com/tencent/ilivedemo/ui/MenuActivity.java
@@ -1,14 +1,17 @@
 package com.tencent.ilivedemo.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tencent.ilivedemo.R;
+import com.tencent.ilivedemo.demos.DemoHost;
 
 /**
  * 示例菜单
  */
-public class MenuActivity extends Activity {
+public class MenuActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +20,17 @@ public class MenuActivity extends Activity {
         setContentView(R.layout.activity_mainmenu);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.lmv_host:
+                enterDemo(DemoHost.class);
+                break;
+        }
+    }
+
+
+    private void enterDemo(Class clsActivity){
+        startActivity(new Intent(this, clsActivity));
+    }
 }
